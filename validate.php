6db9f6dd92b0bbf1b6010473e24b875d54c3471a
matotubol <31701668+matotubol@users.noreply.github.com>
2023-08-20 17:37:42 +0200

    Update validate.php

--- validate.php
+++ validate.php
@@ -37,7 +37,7 @@ function logMessage($message) {
 function validateColor($colorIndex, $paletteId, $indexedPalettes, $hasHabboClub) {
     $palette = $indexedPalettes[$paletteId] ?? null;
     if (!$palette) {
-        logMessage("Failed: palette not found for palette ID '$paletteId'");
+        logMessage("Failed: palette not found for palette ID '$paletteId'\n");
         return false;
     }
 
@@ -46,7 +46,7 @@ function validateColor($colorIndex, $paletteId, $indexedPalettes, $hasHabboClub)
         return true;
     }
 
-    logMessage("Failed: color index '$colorIndex' is invalid or requires Habbo Club");
+    logMessage("Failed: color index '$colorIndex' is invalid or requires Habbo Club\n");
     return false;
 }
 
@@ -56,11 +56,27 @@ function getValidColors($palette, $hasHabboClub) {
     });
 }
 
+function generateNonHCItem($setTypeStr, $gender, $indexedSetTypes, $indexedPalettes) {
+    $setType = $indexedSetTypes[$setTypeStr];
+    $palette = $indexedPalettes[$setType['paletteId']];
+    
+    $validColors = getValidColors($palette, false);
+    $firstValidColor = reset($validColors);
+
+    foreach ($setType['sets'] as $set) {
+        if (($set['gender'] === 'U' || $set['gender'] === $gender) && !$set['club']) {
+            return "$setTypeStr-" . $set['id'] . "-" . $firstValidColor['id'];
+        }
+    }
+
+    return null; // No valid non-HC set found for given setType and gender
+}
+
 function validateSingleItem($item, $gender, $indexedSetTypes, $indexedPalettes, $hasHabboClub) {    
     $splitItem = explode('-', $item);
 
     if (count($splitItem) > 4) {
-        logMessage("Failed: Too many colors specified for item '$item'");
+        logMessage("Failed: Too many colors specified for item '$item'\n");
         return false;
     }
 
@@ -69,36 +85,52 @@ function validateSingleItem($item, $gender, $indexedSetTypes, $indexedPalettes, 
 
     $setType = $indexedSetTypes[$setTypeStr] ?? null;
     if (!$setType) {
-        logMessage("Failed: setType not found for '$setTypeStr'");
+        logMessage("Failed: setType not found for '$setTypeStr'\n");
         return false;
     }
 
     $setItem = $setType['sets'][$setId] ?? null;
     if (!$setItem) {
-        logMessage("Failed: setItem not found for ID '$setId'");
+        logMessage("Failed: setItem not found for ID '$setId'\n");
         return false;
+    }
+
+    if (!$hasHabboClub && $setItem['club']) {
+        // Replace with non-HC set
+        logMessage("Failed: setItem '$setId' is only for HC members\n");
+        return generateNonHCItem($setTypeStr, $gender, $indexedSetTypes, $indexedPalettes);
     }
 
     if ($setItem['gender'] !== 'U' && $setItem['gender'] !== $gender) {
-        logMessage("Failed: gender mismatch for item ID '$setId'");
+        logMessage("Failed: gender mismatch for item ID '$setId'\n");
         return false;
     }
 
-    if ($setItem['club'] && !$hasHabboClub) {
-        logMessage("Failed: setItem ID '$setId' requires Habbo Club");
-        return false;
-    }
-
-    for ($i = 2; $i < count($splitItem); $i++) {
-        $colorIndex = (int) $splitItem[$i];
-        if (!validateColor($colorIndex, $setType['paletteId'], $indexedPalettes, $hasHabboClub)) {
-            return false;
+    // Check if the set is colorable
+    if ($setItem['colorable']) {
+        for ($i = 2; $i < count($splitItem); $i++) {
+            $colorIndex = (int) $splitItem[$i];
+            if (!validateColor($colorIndex, $setType['paletteId'], $indexedPalettes, $hasHabboClub)) {
+                // Replace with first non-HC color
+                $palette = $indexedPalettes[$setType['paletteId']];
+                $validColors = getValidColors($palette, false);
+                $firstValidColor = reset($validColors);
+                $splitItem[$i] = $firstValidColor['id'];
+            }    
         }
+    } else {
+        // If the set isn't colorable, just return the setType-setId format
+        return "$setTypeStr-$setId";
     }
-    return true;
+
+    return implode("-", $splitItem);
 }
 
-function validateMandatorySetTypes($gender, $hasHabboClub, $indexedSetTypes, $encounteredSetTypes) {
+function validateMandatorySetTypes($gender, $hasHabboClub, $indexedSetTypes, &$validatedItems, $indexedPalettes) {
+    $existingSetTypes = array_map(function($item) {
+        return explode('-', $item)[0]; // Extract setType from items
+    }, $validatedItems);
+
     foreach ($indexedSetTypes as $setTypeStr => $setType) {
         $isMandatory = false;
 
@@ -112,40 +144,52 @@ function validateMandatorySetTypes($gender, $hasHabboClub, $indexedSetTypes, $en
                 break;
         }
 
-        // If the setType is mandatory but hasn't been encountered, log an error message
-        if ($isMandatory && !in_array($setTypeStr, $encounteredSetTypes)) {
-            logMessage("Failed: Mandatory setType '$setTypeStr' is missing");
-            return false;
+        // If the setType is mandatory but hasn't been encountered, add the default non-HC item for it
+        if ($isMandatory && !in_array($setTypeStr, $existingSetTypes)) {
+            logMessage("Notice: Mandatory setType '$setTypeStr' is missing. Adding default.");
+            $defaultItem = generateNonHCItem($setTypeStr, $gender, $indexedSetTypes, $indexedPalettes);
+            if ($defaultItem) {
+                $validatedItems[] = $defaultItem;
+            } else {
+                logMessage("Failed: Could not generate a default item for setType '$setTypeStr'");
+                return false;
+            }
         }
     }
     return true;
 }
 
+
+
 function validateItemString($itemString, $gender, $indexedSetTypes, $indexedPalettes, $hasHabboClub) {
     $items = explode('.', $itemString);
+    $validatedItems = [];
     $encounteredSetTypes = []; //track encountered setTypes
 
     foreach ($items as $item) {
         $setType = strstr($item, '-', true); // Extract setType up to the first '-'
 
         // Check if setType has already been encountered
-        if (in_array($setType, $encounteredSetTypes)) {
-            logMessage("Failed: Multiple occurrences of setType '$setType'");
-            return false;
+        if (isset($encounteredSetTypes[$setType])) {
+            logMessage("Skipped: Duplicate setType '$setType'");
+            continue; // skip this item and move to the next one
         }
-        $encounteredSetTypes[] = $setType; // Add setType to the encountered list
 
-        if (!validateSingleItem($item, $gender, $indexedSetTypes, $indexedPalettes, $hasHabboClub)) {
-            return false;
+        $encounteredSetTypes[$setType] = true;
+
+        $validatedItem = validateSingleItem($item, $gender, $indexedSetTypes, $indexedPalettes, $hasHabboClub);
+        if ($validatedItem) {
+            $validatedItems[] = $validatedItem;
         }
     }
-        // After checking all items, validate that all mandatory setTypes are encountered
-        if (!validateMandatorySetTypes($gender, $hasHabboClub, $indexedSetTypes, $encounteredSetTypes)) {
-            return false;
-        }
-            return true;
-}
 
+    // After checking all items, validate that all mandatory setTypes are encountered
+    if (!validateMandatorySetTypes($gender, $hasHabboClub, $indexedSetTypes, $validatedItems, $indexedPalettes)) {
+        return false;
+    }
+
+    return implode(".", $validatedItems);
+}
 
 /////GENERATOR/////
 
@@ -208,19 +252,63 @@ function generateValidLook($gender, $hasHabboClub, $indexedSetTypes, $indexedPal
         return $lookString;
     }
 
-    // If the look is not valid, you can choose to regenerate or return a default look.
-    return "DefaultLook"; // This can be any default valid look you decide upon.
+    // If the look is not valid, generate new look from only the mandatories first item with first color.
+    return generateDefaultLook($gender, $indexedSetTypes, $indexedPalettes);
 }
 
-$randomValidString = generateValidLook('M', false, $indexedSetTypes, $indexedPalettes);
-echo '' . $randomValidString . '"/>';
+function generateDefaultLook($gender, $indexedSetTypes, $indexedPalettes) {
+    $defaultLook = [];
+
+    foreach ($indexedSetTypes as $setTypeStr => $setType) {
+        $isMandatory = false;
+
+        // Determine if the setType is mandatory based on gender
+        switch ($gender) {
+            case 'M':
+                $isMandatory = $setType['mandatory_m_0']; // Assuming non-HC as default
+                break;
+            case 'F':
+                $isMandatory = $setType['mandatory_f_0']; // Assuming non-HC as default
+                break;
+        }
+
+        if ($isMandatory) {
+            $palette = $indexedPalettes[$setType['paletteId']];
+            $validColors = getValidColors($palette, false); // non-HC as default
+
+            if (!empty($validColors)) {
+                $firstColor = reset($validColors);
+                $colorId = $firstColor['id'];
+
+                // Find the first set for this setType that matches the gender
+                foreach ($setType['sets'] as $setId => $set) {
+                    if ($set['gender'] === 'U' || $set['gender'] === $gender) {
+                        // Add this set to the default look
+                        if ($set['colorable']) {
+                            $defaultLook[] = "$setTypeStr-$setId-$colorId";
+                        } else {
+                            $defaultLook[] = "$setTypeStr-$setId";
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    return implode('.', $defaultLook);
+}
+
+// Example usages:
+$gender = 'M'; // or 'F'
+$defaultLookForMale = generateDefaultLook($gender, $indexedSetTypes, $indexedPalettes);
+$gen = generateValidLook($gender, true, $indexedSetTypes, $indexedPalettes);
+
+$randomValidString = generateValidLook('M', true, $indexedSetTypes, $indexedPalettes);
 
 $gender = "M";
-$hasHabboClub = false;
+$hasHabboClub = false; //so you can see how it updates the look when user cant wear that item or color
+echo 'Random generated valid look '.$gen.'<br>';
+echo validateItemString($gen, $gender, $indexedSetTypes, $indexedPalettes, $hasHabboClub);
 
-if (validateItemString($randomValidString, $gender, $indexedSetTypes, $indexedPalettes, $hasHabboClub)) {
-    echo "Valid ";
-} else {
-    echo "Invalid";
-}
 ?>
